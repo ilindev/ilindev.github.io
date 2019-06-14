aa9171789d818adaf5005100ba86d316ac8d9d22
optiumdev <optium.dev@gmail.com>
2019-06-14 16:12:52 +0300

    .

--- js/index.js
+++ js/index.js
@@ -15,3 +15,20 @@ $(function() {
         }
     });
 });
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.main__portfolio_details_content_text_block_share_button_dropright_btn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
